7999845071290dd31c147b62b3e194a262d8e948
Miles Alden <miles.alden@ensighten.com>
2012-11-28 09:49:28 -0800

    Just messing around this morning.

--- Plister/Other Sources/main.m
+++ Plister/Other Sources/main.m
@@ -9,7 +9,38 @@
 #import <Cocoa/Cocoa.h>
 
 
+unsigned long splitInt(unsigned long input) {
+	
+	char ret[100] = { "\x0" };
+    int retSize = sizeof(ret) / sizeof(char);
+    int inputLength = 0;
+    
+	while (input > 0) {
+        char cat[20];
+        sprintf(cat, "%ld", input % 10);
+        input = input / 10;
+        inputLength++;
+		strncat(ret, cat, sizeof(ret) - strlen(ret)-2);
+	}
+    
+
+    int count = inputLength-1;
+    char temp[100] = { "\x0" };
+    for ( int i = 0; i < inputLength; i++ ) {
+        
+        temp[i] = ret[count];
+        count--;
+    }
+    
+	printf("%s", temp);
+	
+	return input;
+}
+
+
 int main(int argc, char *argv[])
 {
+//    splitInt(10023);
+//    return 0;
     return NSApplicationMain(argc, (const char **)argv);
 }
